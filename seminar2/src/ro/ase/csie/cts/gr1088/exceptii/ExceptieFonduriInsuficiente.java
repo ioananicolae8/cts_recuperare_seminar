2d206d9933a35f43816345f3e54e97c223b0f670
ioananicolae8 <ioananicolae12312@gmail.com>
2021-03-04 12:23:47 +0200

    laborator2

---
+++ seminar2/src/ro/ase/csie/cts/gr1088/exceptii/ExceptieFonduriInsuficiente.java
@@ -0,0 +1,11 @@
+package ro.ase.csie.cts.gr1088.exceptii;
+
+public class ExceptieFonduriInsuficiente extends Exception {
+
+	public ExceptieFonduriInsuficiente(String mesaj) {
+	super(mesaj);
+}
+	public ExceptieFonduriInsuficiente() {
+		
+	}
+}
---
+++ seminar2/src/ro/ase/csie/cts/gr1088/exceptii/ExceptieTransferIlegal.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.gr1088.exceptii;
+
+public class ExceptieTransferIlegal extends Exception {
+
+}
