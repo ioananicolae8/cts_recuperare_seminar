f2839669d5e93566d5bc596d3b8de5b5dd9bbebf
ioananicolae8 <ioananicolae12312@gmail.com>
2021-03-25 12:27:16 +0200

    Exemplu Singleton

---
+++ seminar5 - Singleton si Prototype/src/ro/ase/csie/cts/g1080/dp/singleton/ConexiuneBD.java
@@ -0,0 +1,31 @@
+package ro.ase.csie.cts.g1080.dp.singleton;
+
+public class ConexiuneBD {
+	
+	String ip;
+	String denumire;
+	
+	//versiune eager
+	//private static ConexiuneBD conexiune=new ConexiuneBD("10.0.0.1", "cts");
+	
+	private static ConexiuneBD conexiune=null;
+	
+	private ConexiuneBD() {
+		
+	}
+
+	private ConexiuneBD(String ip, String denumire) {
+		System.out.println("Apel constructor");
+		this.ip = ip;
+		this.denumire = denumire;
+	}
+	
+	//versiune lazy
+	public static synchronized ConexiuneBD getConexiune() {
+		if(conexiune==null) {
+			//datele din fisiere de config
+			conexiune=new ConexiuneBD("10.0.0.1", "cts");
+		} 
+		return conexiune;
+	}
+}
---
+++ seminar5 - Singleton si Prototype/src/ro/ase/csie/cts/g1080/dp/singleton/ModulProfil.java
@@ -0,0 +1,8 @@
+package ro.ase.csie.cts.g1080.dp.singleton;
+
+public class ModulProfil {
+	public ModulProfil() {
+		ConexiuneBD conexiune= ConexiuneBD.getConexiune();
+
+	}
+}
