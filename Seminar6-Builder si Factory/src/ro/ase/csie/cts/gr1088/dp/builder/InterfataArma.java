ce94ad7a423b595f6383dabc6945a6e51c3d69b9
ioananicolae8 <ioananicolae12312@gmail.com>
2021-04-01 12:40:49 +0300

    Implementare Builder

---
+++ Seminar6-Builder si Factory/src/ro/ase/csie/cts/gr1088/dp/builder/InterfataArma.java
@@ -0,0 +1,7 @@
+package ro.ase.csie.cts.gr1088.dp.builder;
+
+public interface InterfataArma {
+
+	public abstract void pocPoc();
+	
+}
---
+++ Seminar6-Builder si Factory/src/ro/ase/csie/cts/gr1088/dp/builder/Laser.java
@@ -0,0 +1,5 @@
+package ro.ase.csie.cts.gr1088.dp.builder;
+
+public class Laser implements InterfataSuperPutere{
+
+}
