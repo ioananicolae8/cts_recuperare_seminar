f228bc4484147b4b57e257a25888cf03323c861e
ioananicolae8 <ioananicolae12312@gmail.com>
2021-04-15 19:07:09 +0300

    Implementare Composite

---
+++ seminar8 - Decorator Composite Flyweight/src/ro/ase/csie/cts/g1088/dp/composite/InterfataNPC.java
@@ -0,0 +1,9 @@
+package ro.ase.csie.cts.g1088.dp.composite;
+
+public interface InterfataNPC {
+	
+	public void seDeplaseaza(int x, int y);
+	public void atacaJucator(int puncte);
+	public void seRetrage();
+	
+}
